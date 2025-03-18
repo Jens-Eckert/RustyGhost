89057fd9aa864d4449fcc91630decc096c51e099
Jens-Eckert <capt.lupa@gmail.com>
2025-03-18 15:13:20 -0400

    Initial Commit

---
+++ src/BungieTypes.rs
@@ -0,0 +1,39 @@
+use std::{collections::HashMap, ops::Deref};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
+pub struct GenericResponse<T: Clone> {
+    pub Response: Option<T>,
+    pub ErrorCode: i32,
+    pub ThrottleSeconds: i32,
+    pub ErrorStatus: String,
+    pub Message: String,
+    pub MessageData: HashMap<String, String>,
+    pub DetailedErrorTrace: Option<String>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
+pub struct Destiny2Manifest {
+    pub version: String,
+    pub mobileAssetContentPath: String,
+    pub mobileGearAssetDataBases: Vec<Destiny2ConfigGearAssetDataBaseDefinition>,
+    pub mobileWorldContentPaths: HashMap<String, String>,
+    pub jsonWorldContentPaths: HashMap<String, String>,
+    pub jsonWorldComponentContentPaths: HashMap<String, HashMap<String, String>>,
+    pub mobileClanBannerDatabasePath: String,
+    pub mobileGearCDN: HashMap<String, String>,
+    pub iconImagePyramidInfo: Vec<Destiny2ConfigImagePyramidEntry>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
+pub struct Destiny2ConfigGearAssetDataBaseDefinition {
+    pub version: i32,
+    pub path: String,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone)]
+pub struct Destiny2ConfigImagePyramidEntry {
+    pub name: String,
+    pub factor: f32,
+}
---
+++ src/main.rs
@@ -0,0 +1,80 @@
+use std::collections::HashMap;
+
+use BungieTypes::*;
+use gloo_console::log;
+use gloo_net::http::Request;
+use load_dotenv::load_dotenv;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::JsValue;
+use yew::{html::IntoPropValue, prelude::*};
+
+pub mod BungieTypes;
+
+load_dotenv!();
+
+const API_ROOT: &str = "https://www.bungie.net/Platform";
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+struct AppGlobalData {
+    api_key: String,
+    client_id: String,
+    manifest: Destiny2Manifest,
+}
+
+impl AppGlobalData {
+    fn init() -> AppGlobalData {
+        let api_key = env!("BUNGIE_API_KEY").to_string();
+        let client_id = env!("OAUTH_CLIENT_ID").to_string();
+
+        Self {
+            api_key,
+            client_id,
+            manifest: Destiny2Manifest::default(),
+        }
+    }
+}
+
+#[function_component]
+fn App() -> Html {
+    let mut app_data = AppGlobalData::init();
+
+    let manifest = use_state(|| Destiny2Manifest::default());
+
+    {
+        let manifest = manifest.clone();
+        let mut app_data = app_data.clone();
+
+        use_effect_with((), move |_| {
+            let req = API_ROOT.to_string() + "/Destiny2/Manifest";
+
+            let manifest = manifest.clone();
+            let mut app_data = app_data.clone();
+
+            wasm_bindgen_futures::spawn_local(async move {
+                let response: GenericResponse<Destiny2Manifest> = Request::get(&req)
+                    .header("X-API-Key", &app_data.api_key)
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await
+                    .unwrap();
+
+                app_data.manifest = response.Response.unwrap();
+                manifest.set(app_data.manifest);
+            });
+
+            || ()
+        });
+    }
+
+    html! {
+        <div>
+            <p>{format!("{:?}", *manifest)}</p>
+        </div>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
